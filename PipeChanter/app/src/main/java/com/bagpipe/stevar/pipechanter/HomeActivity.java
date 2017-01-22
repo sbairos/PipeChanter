9f68a59b4f5ccc7c345528580068c2dcad6001bb
Steve Bairos <sbairosnovak@gmail.com>
2017-01-22 17:12:25 -0600

    Adding one sound to all notes. To be worked on later, but will need to add raw folder to git eventually.

--- PipeChanter/app/src/main/java/com/bagpipe/stevar/pipechanter/HomeActivity.java
+++ PipeChanter/app/src/main/java/com/bagpipe/stevar/pipechanter/HomeActivity.java
@@ -1,11 +1,10 @@
 package com.bagpipe.stevar.pipechanter;
 
-import android.annotation.SuppressLint;
-import android.support.v7.app.ActionBar;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.SoundPool;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.os.Handler;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
@@ -24,10 +23,38 @@ public class HomeActivity extends AppCompatActivity {
     private ImmutableBool eCovered = new ImmutableBool(false);
     private ImmutableBool fCovered = new ImmutableBool(false);
 
+    private int gSound;
+    private int aSound;
+
+    MediaPlayer player;
+    SoundPool sounds;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+
+        player = MediaPlayer.create(HomeActivity.this, R.raw.arkells);
+        sounds = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
+
+        sounds.setOnLoadCompleteListener(new SoundPool.OnLoadCompleteListener() {
+            @Override
+            public void onLoadComplete(SoundPool soundPool, int sampleId, int status) {
+                if (status != 0 ) {
+                    System.out.println("FAILED");
+                } else {
+                    System.out.println("WOrked");
+                }
+            }
+        });
+
+
+        aSound = sounds.load(HomeActivity.this, R.raw.arkells, 1);
+        System.out.println("asound = " + aSound);
+//        gSound = sounds.load(HomeActivity.this, R.raw.grouplove, 1);
+
+
+
 
         setListeners();
     }
@@ -35,7 +62,7 @@ public class HomeActivity extends AppCompatActivity {
     private void setListeners() {
 
         Button gButton = (Button) findViewById(R.id.g_finger);
-        gButton.setOnTouchListener(new MyOnTouchListener(gCovered));
+        gButton.setOnTouchListener(new MyOnTouchListener(gCovered, aSound));
 //        gButton.setOnTouchListener(new View.OnTouchListener() {
 //            @Override
 //            public boolean onTouch(View button, MotionEvent event) {
@@ -54,7 +81,7 @@ public class HomeActivity extends AppCompatActivity {
 //        });
 
         Button aButton = (Button) findViewById(R.id.a_finger);
-        aButton.setOnTouchListener(new MyOnTouchListener(aCovered));
+        aButton.setOnTouchListener(new MyOnTouchListener(aCovered, aSound));
 //        aButton.setOnTouchListener(new View.OnTouchListener() {
 //            @Override
 //            public boolean onTouch(View button, MotionEvent event) {
@@ -89,7 +116,7 @@ public class HomeActivity extends AppCompatActivity {
 //                return false;
 //            }
 //        });
-        bButton.setOnTouchListener(new MyOnTouchListener(bCovered));
+        bButton.setOnTouchListener(new MyOnTouchListener(bCovered, aSound));
 
         Button cButton = (Button) findViewById(R.id.c_finger);
 //        cButton.setOnTouchListener(new View.OnTouchListener() {
@@ -110,17 +137,17 @@ public class HomeActivity extends AppCompatActivity {
 //                return false;
 //            }
 //        });
-        cButton.setOnTouchListener(new MyOnTouchListener(cCovered));
+        cButton.setOnTouchListener(new MyOnTouchListener(cCovered, aSound));
 
         Button dButton = (Button) findViewById(R.id.d_finger);
-        dButton.setOnTouchListener(new MyOnTouchListener(dCovered));
+        dButton.setOnTouchListener(new MyOnTouchListener(dCovered, aSound));
 
         Button eButton = (Button) findViewById(R.id.e_finger);
-        eButton.setOnTouchListener(new MyOnTouchListener(eCovered));
+        eButton.setOnTouchListener(new MyOnTouchListener(eCovered, aSound));
 
 
         Button fButton = (Button) findViewById(R.id.f_finger);
-        fButton.setOnTouchListener(new MyOnTouchListener(fCovered));
+        fButton.setOnTouchListener(new MyOnTouchListener(fCovered, aSound));
 
     }
 
@@ -145,14 +172,47 @@ public class HomeActivity extends AppCompatActivity {
 //        }
         System.out.println("G is " + gCovered);
         System.out.println("WTF");
+
+
+//        MediaPlayer player = MediaPlayer.create(HomeActivity.this, R.raw.arkells);
+//        try {
+//            wait(100);
+//            if (gCovered.getValue()) {
+//                System.out.println("playing");
+//                player.start();
+//            } else if (player.isPlaying()) {
+//                System.out.println("stopping");
+//                player.pause();
+//            }
+//        } catch (InterruptedException e) {
+//            System.out.println("Interrupted");
+//        }
         // Here's where we're going to continually check the status of the buttons that are pressed.
     }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        sounds.release();
+    }
+
+    public void chooseNote() {
+        /*
+        This is tough to choose whether or not to choose the note from top down or do a divide and conquer thing?
+        Since top down is at worst 9 if statements and the divide and conquer is average case 3.5 ifs but worst case
+        would be more because of grace notes
+         */
+    }
 
     class MyOnTouchListener implements View.OnTouchListener{
-        ImmutableBool bool;
-        public MyOnTouchListener(ImmutableBool bool) {
+        private ImmutableBool bool;
+        private int mediaId;
+        private int currStreamId = 0;
+
+        public MyOnTouchListener(ImmutableBool bool, int mediaId) {
             this.bool = bool;
+            System.out.println("ID = " + mediaId);
+            this.mediaId = mediaId;
         }
 
         @Override
@@ -161,10 +221,12 @@ public class HomeActivity extends AppCompatActivity {
                 case MotionEvent.ACTION_DOWN:
                     bool.setValue(true);
                     System.out.println(button.getId() +" is " + bool.getValue());
+                    currStreamId = sounds.play(mediaId, 1, 1, 0, 0, 1);
                     return true;
                 case MotionEvent.ACTION_UP:
                     bool.setValue(false);
                     System.out.println(button.getId() +" is " + bool.getValue());
+                    sounds.stop(currStreamId);
                     return true;
             }
             return false;
